81fde67228297c5be1021902f0fb4e5ed7b4f61e
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2017-11-28 14:27:42 +0700

    searchbar done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,6 +11,8 @@ import { ContentPage } from '../content/content';
 export class HomePage {
 
   list: any;
+  list_search: any;
+  search = false;
 
   constructor(
     public navCtrl: NavController,
@@ -43,6 +45,33 @@ export class HomePage {
        console.log(this.list);
        loading.dismiss();
     });
+  }
+
+  //Fungsi Searchbar
+  getItems(ev) {
+    this.search=true;
+
+    // Reset items back to all of the items
+    this.list_search = this.list;
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.list_search = this.list_search.filter((item) => {
+        return (item.data.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+      // this.list_search = this.list_search.filter((data) => {
+      //   return (data.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      // })
+    }
+    else {
+      this.search=false;
+    }
+
+    console.log(this.list_search);
+    console.log("search="+this.search);
   }
 
 }
